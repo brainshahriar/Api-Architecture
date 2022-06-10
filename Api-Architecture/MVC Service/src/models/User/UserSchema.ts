f20a19076014c6d44be70b3a71ca783a517c873d
shahriar <shahriar.mehedi@bjitgroup.com>
2022-06-10 17:17:39 +0600

    interfaces

--- Api-Architecture/MVC Service/src/models/User/UserSchema.ts
+++ Api-Architecture/MVC Service/src/models/User/UserSchema.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import User from "../../interfaces/UserInterfaces";
+import User from "./UserInterface";
 
 const userSchema = new mongoose.Schema({
         title: { type: String, required: true, trim: true },
